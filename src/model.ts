1a0fbc64394f4790479caae30c3d8cc75aa14038
Nav989 <94343020+Nav989@users.noreply.github.com>
2022-01-13 18:52:11 +0530

    Update model.ts

--- src/model.ts
+++ src/model.ts
@@ -10,48 +10,53 @@ const test= async(req:Request,res:Response)=>{
 
 const create = async (req:Request,res:Response)=>{
    console.log('1');
-
-let name = req.body.name;
-  let email = req.body.email;
-  let product = req.body.product;
-  let product_qty = req.body.product_qty;
-    const createData = await UserModel.create({name,email,product,product_qty
+let item:UserInstance=req.body;
+const findemail = await UserModel.findOne({
+    where: {
+        email:item.email,
+    }
 });
+console.log("email  "  + findemail?.email)
+if(findemail){
+    res.status(400).send({message: "email already present"})
 
-    // await createData.save()
+}else{
+    const createData = await UserModel.create(item);
+
      if(createData){
         res.status(201).send(createData);
        console.log("inserted") 
      } else{
         res.status(400).send({message: "error"})
      }
-}
+    } 
+ }
 
 
 
 
 const read = async (req:Request,res:Response) => {
 
-    // const users = await Users.query("SELECT * FROM `crudtables`", { type: QueryTypes.SELECT });
-
     const userData = await UserModel.findAll({attributes:[ "id","name","email","product","product_qty"]});
 
-
     if(userData)res.status(200).send(userData); 
-    res.status(400).send({message: "error"})
+    else{
+        res.status(400).send({message: "error"});
+    }
 }
 
 
 const update = async (req:Request,res:Response) => {
 
-    const updateData = await UserModel.update({
-        name: req.body.name,
-        product:req.body.product,
-        product_qty:req.body.product_qty,
-        
-    },{
+    let item:UserInstance=req.body;
+
+    const updateData = await UserModel.update(item
+        // {name:item.name,
+        // product:item.product,
+        // Product:item.product_qty}
+    ,{
         where: {
-            email: req.body.email,
+            email: item.email,
         }
 
     });
@@ -67,14 +72,16 @@ const update = async (req:Request,res:Response) => {
 
 const deletedata = async (req:Request,res:Response) => {
 
-    const deleteData= await UserModel.destroy({where:{name:req.body.name}})
+    let item:UserInstance=req.body;
+
+    const deleteData= await UserModel.destroy({where:{name:item.name}})
 
     console.log(deleteData)
     if(deleteData){
         console.log("delete")
-        res.status(200).sendStatus(deleteData);    
+        res.status(200).json(deleteData);    
     }else{
-        res.status(400).send({message: "not deleted"})
+        res.status(400).json({message: "not deleted"})
     }
 
 }
